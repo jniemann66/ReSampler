09ba1dd1c3defcaa4d6420e7b135bdcab80f5c73
jniemann66 <jniemann666@bigpond.com>
2020-09-11 21:31:24 +1000

    added copyright notices

--- effectchain.h
+++ effectchain.h
@@ -1,3 +1,12 @@
+/*
+* Copyright (C) 2020 Judd Niemann - All Rights Reserved.
+* You may use, distribute and modify this code under the
+* terms of the GNU Lesser General Public License, version 2.1
+*
+* You should have received a copy of GNU Lesser General Public License v2.1
+* with this file. If not, please refer to: https://github.com/jniemann66/ReSampler
+*/
+
 // effectchain.h : defines container for effects
 // if takeOwnership is set to true, effectchain will delete the effects upon destruction
 
